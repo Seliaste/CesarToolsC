fd9ed0c098c4f4053eddce8f484a6d2eeca7701c
= <=>
2022-03-09 11:11:23 +0100

    Implementation de Vigenere

---
+++ Vigenere/VigenereTest.c
@@ -0,0 +1,18 @@
+#include "Vigenere.h"
+
+void testVigener()
+{
+    printf("B + a = %c\n", vigener_char('B', 'a', false));
+    printf("o + b = %c\n", vigener_char('o', 'b', false));
+    printf("n + c = %c\n", vigener_char('n', 'c', false));
+    printf("u + f = %c\n", vigener_char('u', 'e', false));
+    printf("u + f = %c\n", vigener_char('u', 'f', false));
+    printf("Bonjour Madame + abc = %s\n", chiffrer_vigener("Bonjour Madame", "abc"));
+    printf("Cqqkqxs Pbfdng - abc = %s\n", dechiffrer_vigener("Cqqkqxs Pbfdng", "abc"));
+}
+
+int main(int argc, char const *argv[])
+{
+    testVigener();
+    return 0;
+}
