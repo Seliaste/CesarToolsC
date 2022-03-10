0fb394c36af57e4e95b5f9712176fa8ee9b38047
= <=>
2022-03-10 17:27:01 +0100

    Added vigenere bruteforce and fixed cesar bruteforce

--- Espion/Espion.c
+++ Espion/Espion.c
@@ -2,14 +2,41 @@
 
 char *casser_cesar(char *texte)
 {
-    int* tab = tableau_frequence(texte);
+    int *tab = tableau_frequence(texte);
     int max = 0;
     char max_char;
-    for(int i=0; i < 26; i++){
-        if(tab[i]>max){
+    for (int i = 0; i < 26; i++)
+    {
+        if (tab[i] > max)
+        {
             max = tab[i];
             max_char = i;
         }
     }
-    return dechiffrementCesar(texte,max_char+92);
+    if (max_char + 92 < 97)
+    {
+        max_char += 26;
+    }
+    return dechiffrementCesar(texte, max_char + 92);
+}
+
+char *casser_vigenere_avec_clef(char *texte, int tailleClef)
+{
+    char *res = calloc(taille(texte), sizeof(char));
+    for (int i = 0; i < tailleClef; i++)
+    {
+        char *subString = calloc(taille(texte), sizeof(char));
+        int j;
+        for (j = 0; j < taille(texte) / 3; j++)
+        {
+            subString[j] = texte[i + j * 3];
+        }
+        char *decodedSubString = casser_cesar(subString);
+        for (j = 0; j < taille(texte) / 3; j++)
+        {
+            res[i + j * 3] = decodedSubString[j];
+        }
+        free(subString);
+    }
+    return res;
 }
--- Espion/EspionTest.c
+++ Espion/EspionTest.c
@@ -1,13 +1,21 @@
 #include "Espion.h"
 
-void testEspion(){
+void testEspion()
+{
     printf("Iwtivpyixxi = %s\n", casser_cesar("Iwtivpyixxi"));
-    char* texte_long = lecture("exemple1_chiffre_cesar.txt");
-    printf("%s\n",casser_cesar(texte_long));
+    char *texte_long = lecture("exemple1_chiffre_cesar.txt");
+    printf("%s\n", casser_cesar(texte_long));
+}
+
+void testCasserVigenere()
+{
+    char *texte_long = lecture("exemple3_vigenere_clef_taillle_3.txt");
+    printf("%s\n", casser_vigenere_avec_clef(texte_long, 3));
 }
 
 int main(int argc, char const *argv[])
 {
     testEspion();
+    testCasserVigenere();
     return 0;
 }
