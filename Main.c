41204f301f5fbc50b1b7cfbc7e66349bbd361728
= <=>
2022-03-11 15:56:23 +0100

    Added feedback on wrong arguments

--- Main.c
+++ Main.c
@@ -7,30 +7,35 @@
 #include "Vigenere/Vigenere.c"
 int main(int argc, char *argv[])
 {
-    if (strcmp("-c", argv[1])==0)
+    if (argc <= 1)
+    {
+        printf("Syntaxe incorrecte.\nUsage :\nmain -c <nomFichier> <clef> : code le fichier\nmain -d <nomFichier> <clef> : décode le fichier\n main <nomFichier> : casse le chiffrage en césar puis en vigénère.\n");
+        exit(EXIT_SUCCESS);
+    }
+    if (strcmp("-c", argv[1]) == 0)
     {
         if (taille(argv[3]) == 1)
         {
-            printf("%s",chiffrementCesar(lecture(argv[2]), argv[3][0]));
+            printf("%s", chiffrementCesar(lecture(argv[2]), argv[3][0]));
         }
         if (taille(argv[3]) > 1)
         {
-            printf("%s",chiffrer_vigener(lecture(argv[2]), argv[3]));
+            printf("%s", chiffrer_vigener(lecture(argv[2]), argv[3]));
         }
         else
         {
             EXIT_FAILURE;
         }
     }
-    else if (strcmp("-d", argv[1])==0)
+    else if (strcmp("-d", argv[1]) == 0)
     {
         if (taille(argv[3]) == 1)
         {
-            printf("%s",dechiffrementCesar(lecture(argv[2]), argv[3][0]));
+            printf("%s", dechiffrementCesar(lecture(argv[2]), argv[3][0]));
         }
         if (taille(argv[3]) > 1)
         {
-           printf("%s",dechiffrer_vigener(lecture(argv[2]), argv[3]));
+            printf("%s", dechiffrer_vigener(lecture(argv[2]), argv[3]));
         }
         else
         {
@@ -39,7 +44,7 @@ int main(int argc, char *argv[])
     }
     else
     {
-        printf("%s\n\n\n\n",casser_cesar(lecture(argv[1])));
-        printf("%s",casser_vigenere_avec_clef(lecture(argv[1]),3));
+        printf("%s\n\n\n\n", casser_cesar(lecture(argv[1])));
+        printf("%s", casser_vigenere_avec_clef(lecture(argv[1]), 3));
     }
 }
